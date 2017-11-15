b9a6d009061ca5af5219b5447a710d1facd64a8b
ebataitis <ebataitis@gmail.com>
2017-11-15 11:18:44 +0200

    first commit

---
+++ dist/contacts.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<?php include 'parts/head.php'?>
+
+<body>
+    <div class="heading">
+        <div class="container">
+            <h2>PROJECTS</h2>
+            <h3>Home>Projects</h3>
+
+        </div>
+    </div>
+    <div class="contact-form container">
+        <h1>CONTACT US</h1>
+        <div class="under-line"></div>
+        <div class="inputs">
+            <input type="text" placeholder="First Name">
+            <input type="text" placeholder="Last Name">
+            <input type="text" placeholder="Your E-Mail">
+            <input type="text" placeholder="Website">
+            <textarea name="" id="message" cols="30" rows="10" placeholder="Message"></textarea>
+            <input type="submit" class="submit" value="SEND">
+        </div>
+    </div>
+    <div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d5464619.764369678!2d88.47570691044199!3d23.24013186196639!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1slt!2slt!4v1510697182778" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe></div>
+    <div class="get-in-touch">
+        <div class="container">
+            <h2>GO AHEAD AND FIND OUT HOW WE CAN HELP YOU!</h2>
+            <button>GET IN TOUCH</button></div>
+    </div>
+</body>
+
+</html>
---
+++ dist/index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+<?php include 'parts/head.php'?>
+
+<body class="home-page">
+    <header>
+        <div class="top-menu">
+            <div class="container">
+                <nav class="social">
+                    <ul>
+                        <li><a href="">x</a></li>
+                        <li><a href="">x</a></li>
+                        <li><a href="">x</a></li>
+                        <li><a href="">x</a></li>
+                    </ul>
+                </nav>
+                <nav class="contacts">
+                    <ul>
+                        <li>Mon - Sat 9.00 - 17.00</li>
+                        <li>
+                            <a href="tel:+6494461709">+6494461709</a>
+                        </li>
+                        <li>
+                            <a href="mailto:info@gmail.com"></a>info@gmail.com
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <div class="main-menu container">
+            <img src="" alt="logo">
+            <nav>
+                <ul>
+                    <li><a href="">home</a></li>
+                    <li><a href="">about</a></li>
+                    <li><a href="">pages</a></li>
+                    <li><a href="">projects</a></li>
+                    <li><a href="">blog</a></li>
+                    <li><a href="./contacts.php" target="_blank">contact us</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</body>
+
+</html>
